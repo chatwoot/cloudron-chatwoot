f9dac2aaac3a2fb39e62570b1776ea1fea438fb9
Vishnu Narayanan <vishnu@chatwoot.com>
2021-09-10 18:56:23 +0530

    fix redis creds

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 set -x
 
-echo "==> Starting supervisor"
+sed -e "s#REDIS_URL#redis://user:${CLODURON_REDIS_PASSWORD}@${CLODURON_REDIS_HOST}:${CLODURON_REDIS_PORT}#" \
+    -e "s#REDIS_PASSWORD#${CLODURON_REDIS_PASSWORD}#" \
+    /app/config/cable.yaml
 
+
+echo "==> Starting supervisor"
 exec /usr/bin/supervisord --configuration /etc/supervisor/supervisord.conf
